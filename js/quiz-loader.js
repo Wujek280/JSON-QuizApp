b22e3e874ad25a17be7cc770cb8a1f071ce0473c
Wujek280 <wujek5151@gmail.com>
2017-04-28 20:55:42 +0200

    REALLY WORKING JSON, minutes, questions

--- js/quiz-loader.js
+++ js/quiz-loader.js
@@ -1,29 +1,30 @@
 'use strict';
 
-var quiz;
+/////////OBTAINING QUIZ FROM URL
+////////////////////////////////
 
 var path = 'https://cdn.rawgit.com/kdzwinel/cd08d08002995675f10d065985257416/raw/811ad96a0567648ff858b4f14d0096ba241f28ef/quiz-data.json';
 
 /* STACK OVERFLOW */ 
 
-function fetchJSONFile(path, callback) {
-    var httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = function() {
-        if (httpRequest.readyState === 4) {
-            if (httpRequest.status === 200) {
-                var data = JSON.parse(httpRequest.responseText);
-                if (callback) callback(data);
-            }
-        }
-    };
-    httpRequest.open('GET', path);
-    httpRequest.send(); 
-}
+function readJSON(file) {
+    var request = new XMLHttpRequest();
+    request.open('GET', file, false);
+    request.send(null);
+    if (request.status == 200)
+        return request.responseText;
+};
 
-fetchJSONFile(path, function(data){
-    quiz = data;
-});
+var quiz = JSON.parse(readJSON(path));
 
+/*     ***     */
 
+/////////NUMBERS FOR MAIN PAGE 
+//////////////////////////////
 
+var quizNoOfQuestions = quiz.questions.length;
+var quizTimeMinutes = parseInt(quiz.time_seconds / 60);
+var quizTimeSeconds = parseInt(quiz.time_seconds % 60);
+
+console.log();
 
