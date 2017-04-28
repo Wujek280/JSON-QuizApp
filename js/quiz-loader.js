a8d683e77f829d5f7246be5de8dffdb745f6eca1
Wujek280 <wujek5151@gmail.com>
2017-04-28 20:14:56 +0200

    LOADING QUIZ URL

--- js/quiz-loader.js
+++ js/quiz-loader.js
@@ -1,8 +1,29 @@
 'use strict';
 
+var quiz;
 
-// HERE GOES THE JSON PARSER
-// BUT YOU JUST ATTACH IT AS IT IS
-// YOU RE LAZY MARCIN!
+var path = 'https://cdn.rawgit.com/kdzwinel/cd08d08002995675f10d065985257416/raw/811ad96a0567648ff858b4f14d0096ba241f28ef/quiz-data.json';
+
+/* STACK OVERFLOW */ 
+
+function fetchJSONFile(path, callback) {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) {
+                var data = JSON.parse(httpRequest.responseText);
+                if (callback) callback(data);
+            }
+        }
+    };
+    httpRequest.open('GET', path);
+    httpRequest.send(); 
+}
+
+fetchJSONFile(path, function(data){
+    quiz = data;
+});
+
+
 
 
