8613be7a7e6dc3d5c05802b79128062ed42dbccb
Wujek280 <mjwojcik94@gmail.com>
2017-05-01 02:05:47 +0200

    update

--- js/quiz-loader.js
+++ js/quiz-loader.js
@@ -56,14 +56,16 @@ var quizNoOfQuestions = quiz.questions.length;
 ////////       USER STATS
 //////////////////////////////////
 
+//ARRAY OF USER ANSWERS
 var answers = [];
 
-var Answer = function(id, question, correct, user, scored){
+//ANSWER OBJECT DEFINITION
+var Answer = function(id, question, correctAns, userAns, scored){
    this.id = id;
    this.question = question;
-   this.correctAnswer = correct;
-   this.userAnswer = user;
-   this.userScored = scored;
+   this.correctAnswer = correctAns || ' . . . ';
+   this.userAnswer = userAns || ' ... ';
+   this.userScored = scored || '-';
 };
 
 //////////////////////////////////
@@ -75,6 +77,8 @@ function setUp() {
    //////  INVOKE FIRST QUESTION
    showQuestion(currentQuestionID);
    
+
+   /////   START COUNTING THETIME
    var date = new Date();
    
    var startTime = date.getTime();
@@ -160,16 +164,14 @@ function showQuestion(n) {
          quiz.questions[n].answers.forEach(function(element, i){
 
             //CREATE ANSWER NODES//
-            var createAnswer;
+            var createAnswer = document.createElement('input')
                i++;
-               createAnswer = document.createElement('input');
                createAnswer.type = 'radio';
                createAnswer.setAttribute('name','answer');
                createAnswer.id = 'ans_'+i;
 
 
-            var createLabelForAnswer;
-               createLabelForAnswer = document.createElement('label');
+            var createLabelForAnswer = document.createElement('label');
                createLabelForAnswer.setAttribute('for','ans_'+i );
                createLabelForAnswer.className = "quiz-bar-answer quiz-bar";   
                createLabelForAnswer.innerHTML = element.answer;
@@ -178,7 +180,7 @@ function showQuestion(n) {
             document.getElementById('quiz').appendChild(createAnswer);   
             document.getElementById('quiz').appendChild(createLabelForAnswer);   
 
-            //ADD PROPER ONCLICK LISTENER//
+            //ADD PROPER ONCLICK LISTENER FOR EACH BUTTON//
             document.getElementById('ans_'+i).addEventListener('click',function(){
                console.log(i);
                userAnswerID = i;
@@ -216,9 +218,9 @@ function answerQuestion(n){
             }
       });
       
-      ///SAVE USER ANSWER AS TXT
+      ///SAVE USER ANSWER AS STRING 
       var userAnswer = quiz.questions[n].answers[userAnswerID-1];
-
+   
       ///// IF USER SCORED +/- THEN RESET TO null
       if(userAnswer.correct)
       {
@@ -267,7 +269,7 @@ function answerQuestion(n){
 
 function endOfQuiz(timeLeft) { 
    
-   if(timeLeft === false) console.log('KONIEC CZASU');
+   if(timeLeft === true) console.log('KONIEC CZASU');
    else console.log('KONIEC');
    
    /// UPDATE HEADER
@@ -283,70 +285,58 @@ function endOfQuiz(timeLeft) {
    ///////////////
    document.getElementById("quiz-submit-label").outerHTML = '';
    
-   ////////////////////////
    ////CREATE TABLE
    ////////////////////////
-   
    var table = document.createElement('table');
    table.className = 'quiz-table';
-   
-   
+    
    var Container = document.getElementById("quiz-box");
    Container.innerHTML = '';
    
    /////////////////////////////////////////////////////
    /////////////////////////////////////////////////////
    /////////////////////////////////////////////////////
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
+       
    /////////////////////////////////////////////////////
    /////////////////////        ////////////////////////
    ///////////////                   ///////////////////
    answers.forEach(function(element){
    
-      //// CREATE ELEMENTS 
+      //// CREATE DIV FOR EACH QUESTION 
       var div = document.createElement('div');
 
+      //// ADD PROPER CSS CLASS
       if(element.userScored == '+'){
-         div.className = 'feedback--good';         
+         div.className = 'feedback feedback--good';         
       }else{
-         div.className = 'feedback--bad';
+         div.className = 'feedback feedback--bad';
       }
 
-      var hr = document.createElement('hr');
+      //// APPEND DIV
+      Container.appendChild(div);
+      var currentDiv = Container.getElementsByTagName('div')[0];
 
-      var p1 = document.createElement('p');
-      var p2 = document.createElement('p');
-      var p3 = document.createElement('p');
+      var parQuestion = document.createElement('p');
+      var parUserAnswer = document.createElement('li');
+      var parCorrectAnswer = document.createElement('li');
       
       //// ASSIGN VALUES 
-      p1.innerHTML = element.id+'. '+element.question ;
-      p2.innerHTML = ' >'+element.userAnswer;
-      p3.innerHTML = ' >'+element.correctAnswer;
+      parQuestion.innerHTML = element.id+'. '+element.question ;
+      parUserAnswer.innerHTML = ' '+element.userAnswer;
+      parCorrectAnswer.innerHTML = ' '+element.correctAnswer;
       
       
-      //// APPEND
-      div.append(p1);
+      //// APPEND (line 329)
+      currentDiv.appendChild(parQuestion);
+
       if(element.userScored == '-'){
-         p2.className = 'feedback-p--crossed';
-         div.append(p2);         
-         div.append(p3);
+         parUserAnswer.className = 'feedback-p--crossed';
+         currentDiv.appendChild(parUserAnswer);         
+         currentDiv.appendChild(parCorrectAnswer);
       }else{
-         div.append(p2);
+         currentDiv.appendChild(parUserAnswer);
       }
       
-      div.append(hr);
-      
-      Container.append(div);
-            
    });
    ///////////////                 ////////////////////
    ////////////////////      //////////////////////////
